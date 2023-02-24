a4ea4013e55a06af2df7809d9a4ace414b000515
omrijsharon <omri_s@yahoo.com>
2023-02-24 17:12:00 +0200

    testing the model

--- src/core/model.py
+++ src/core/model.py
@@ -7,23 +7,25 @@ from gym.spaces import Box, Discrete
 
 
 class TransformerRL(nn.Module):
-    def __init__(self, state_space, action_space, d_model, nhead, num_layers, dropout):
+    def __init__(self, obs_space, action_space, d_model, nhead, num_layers, dropout):
         super(TransformerRL, self).__init__()
+        # Positional encoding
+        self.pos_encoder = nn.Linear(2, self.d_model)
         # Embedding layers for action
         self.d_model = d_model
         self.action_dim = None
         if isinstance(action_space, Box):
-            self.action_dim = np.prod(action_space.shape)
+            self.action_dim = np.prod(action_space.shape).astype(int).item()
             self.action_embedding = nn.Linear(self.action_dim, self.d_model)
         elif isinstance(action_space, Discrete):
             self.action_dim = action_space.n
             self.action_embedding = nn.Embedding(self.action_dim, self.d_model)
-        # Embedding layers for state
-        self.state_dim = np.prod(state_space.shape)
-        self.state_embedding = nn.Linear(self.state_dim, self.d_model)
+        # Embedding layers for obs
+        self.obs_space = np.prod(obs_space.shape).astype(int).item()
+        self.obs_embedding = nn.Linear(self.obs_space, self.d_model)
 
         # BatchNorm layers
-        self.state_bn = nn.BatchNorm1d(self.d_model)
+        self.obs_bn = nn.BatchNorm1d(self.d_model)
         self.action_bn = nn.BatchNorm1d(self.d_model)
 
         # Dropout layers
@@ -38,23 +40,23 @@ class TransformerRL(nn.Module):
         self.transformer_decoder = nn.TransformerDecoder(
             nn.TransformerDecoderLayer(d_model=2*self.d_model, nhead=nhead),
             num_layers=num_layers)
-        # Linear layer for decoding the next state
-        self.decoder_next_state = nn.Linear(2*self.d_model, self.state_dim)
+        # Linear layer for decoding the next obs
+        self.decoder_next_obs = nn.Linear(2 * self.d_model, self.obs_space)
         self.decoder_next_action = nn.Linear(2*self.d_model, self.action_dim)
 
-    def forward(self, state, action):
-        # Embed the state and action
-        batch_size, seq_len, state_dim = state.shape
+    def forward(self, obs, action):
+        # Embed the obs and action
+        batch_size, seq_len, obs_dim = obs.shape
         batch_size, seq_len, action_dim = action.shape
-        state_embedded = self.state_embedding(state.reshape(-1, state_dim))
-        action_embedded = self.action_embedding(action.reshape(-1, action_dim))
+        obs_embedded = self.obs_embedding(obs.view(-1, obs_dim))
+        action_embedded = self.action_embedding(action.view(-1, action_dim))
 
         # Apply batch norm to the embeddings
-        state_embedded = self.state_bn(state_embedded).reshape(batch_size, seq_len, -1)
-        action_embedded = self.action_bn(action_embedded).reshape(batch_size, seq_len, -1)
+        obs_embedded = self.obs_bn(obs_embedded).view(batch_size, seq_len, -1)
+        action_embedded = self.action_bn(action_embedded).view(batch_size, seq_len, -1)
 
         # Concatenate the embeddings along the time dimension
-        x = torch.cat([state_embedded, action_embedded], dim=2)
+        x = torch.cat([obs_embedded, action_embedded], dim=2)
 
         # Apply dropout
         x = self.dropout1(x)
@@ -72,9 +74,17 @@ class TransformerRL(nn.Module):
         # Transformer decoder
         tgt_embedded = self.transformer_decoder(tgt, encoded_src)
 
-        # Decode next state and action
-        decoded_state = self.decoder_next_state(tgt_embedded)
+        # Decode next obs and action
+        decoded_obs = self.decoder_next_obs(tgt_embedded)
         decoded_action = self.decoder_next_action(tgt_embedded)
 
-        return decoded_state, decoded_action
+        return decoded_obs, decoded_action
 
+if __name__ == '__main__':
+    import gym
+    env = gym.make('CartPole-v1')
+    obs_space = env.observation_space
+    action_space = env.action_space
+    model = TransformerRL(obs_space, action_space, d_model=64, nhead=4, num_layers=2, dropout=0.1)
+    print(model)
+    # F.gelu(approximate="tanh")
